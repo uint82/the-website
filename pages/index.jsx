8d982756f6160f78d73a4fb47cbe14c73798eb91
uint82 <abrorhilmi6@gmail.com>
2025-12-27 20:14:10 +0700

    feat: added new function Visitor, chore: remove double gabut button

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,15 @@ import AnimatedMessage from "../components/AnimatedMessage";
 import DelayCursor from "../components/DelayCursor";
 
 export default function Index() {
+  function Visitor() {
+    return (
+      <div>
+        <a href="https://flagcounter.me/details/gt8">
+          <img src="https://flagcounter.me/gt8/" alt="Flag Counter" />
+        </a>
+      </div>
+    );
+  }
   return (
     <div>
       <h2>Selamat datang!</h2>
@@ -11,31 +20,34 @@ export default function Index() {
           <button>MULAI</button>
         </Link>
         <Link href="/nbrthx">
-          <button style={{ marginLeft: '5px' }}>SVGSTEGO</button>
+          <button style={{ marginLeft: "5px" }}>SVGSTEGO</button>
         </Link>
         <Link href="/doom">
-          <button style={{ marginLeft: '5px', backgroundColor: 'red', color: 'white' }}>
+          <button
+            style={{
+              marginLeft: "5px",
+              backgroundColor: "red",
+              color: "white",
+            }}
+          >
             DOOM
           </button>
         </Link>
         <Link href="/vinrex/index.html">
-          <button style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <button style={{ display: "flex", alignItems: "center", gap: "8px" }}>
             <img
               src="/vinrex/assets/default_100_percent/100-error-offline.png"
               alt="Vinrex Icon"
-              style={{ width: '24px', height: '24px' }}
+              style={{ width: "24px", height: "24px" }}
             />
             VINREX GAME
           </button>
         </Link>
-        <Link href="/gabut">
-          <button>gabut</button>
-        </Link>
-         <Link href="/snekabsurd">
-          <button style={{ marginLeft: '5px' }}>Absurd Snek Gem</button>
+        <Link href="/snekabsurd">
+          <button style={{ marginLeft: "5px" }}>Absurd Snek Gem</button>
         </Link>
         <Link href="/bola-ajaib">
-            <button>Bola ajaib</button>
+          <button>Bola ajaib</button>
         </Link>
         <Link href="/gabut">
           <button>gabut</button>
@@ -43,6 +55,7 @@ export default function Index() {
       </p>
       <AnimatedMessage />
       <DelayCursor delay={0.05} />
+      <Visitor />
       <style jsx global>{`
         * {
           cursor: none !important;
